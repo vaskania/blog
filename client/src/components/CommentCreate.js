c60deef5ff315d2194a4f0a4d500e3b500e17311
vaska <vaskania26@gmail.com>
2022-04-27 21:33:06 +0400

    blog begin

---
+++ client/src/components/CommentCreate.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import axios from 'axios';
+
+const CommentCreate = ({postId}) => {
+    const [content, setContent] = useState('')
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        })
+
+        setContent('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <label>New Comment</label>
+                    <input
+                        className='form-control'
+                        value={content}
+                        onChange={e => setContent(e.target.value)}
+                    />
+                </div>
+                <button className='btn btn-primary' style={{marginTop: "10px"}}>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default CommentCreate;
