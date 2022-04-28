a5d2e6e9080abbe898628158ee92ed2e73b3d05e
vaskania <vnikabadze@unisens.ge>
2022-04-28 16:51:38 +0400

    added done 2nd module

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,21 +1,19 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios'
-
-const CommentList = ({postId}) => {
-    const [comments, setComments] = useState([])
-
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(res.data)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, []);
-
+const CommentList = ({ comments }) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content
+
+        if (comment.status === 'approved') {
+            content = comment.content
+        }
+        if (comment.status === 'pending') {
+            content = 'This comment is waiting moderation.'
+        }
+        if (comment.status === 'rejected') {
+            content = 'This comment has been rejected.'
+        }
+
+
+        return <li key={comment.id}>{content}</li>
     })
 
     return (
